fix(model): use broadcasting in spatial_dense

vit_jax/models_mixer.py
@@ -37,21 +37,21 @@ class MixerBlock(nn.Module):
     tokens_mlp_dim: int
     channels_mlp_dim: int
 
-    def _swap_dims(self, inp, shape):
-        out = nn.LayerNorm()(inp)
+    def _spatial_dense(self, inp, shape):
         weight = self.param(f'kernel{shape[0]}', init, shape)
-        out = lax.dot_general(weight, out, (((0,), (1,)), ((), (0,))))
+        weight = lax.broadcast(weight, (inp.shape[0],))
+        out = lax.dot_general(weight, inp, (((1,), (1,)), ((0,), (0,))))
         out += self.param(f'bias{shape[0]}', jax.nn.initializers.zeros,
-                          (1, 1, shape[1]))
+                          (1, shape[-1], 1))
         return out
 
     @nn.compact
     def __call__(self, x):
         _, spatial, feature = x.shape
         shape = (spatial, self.tokens_mlp_dim)
-        x += nn.Dense(spatial)(nn.gelu(self._swap_dims(x, shape)))
-        x += nn.Dense(feature)(nn.gelu(self._swap_dims(x, shape)))
-        return x
+        y = self._spatial_dense(nn.LayerNorm()(x), shape)
+        x += self._spatial_dense(nn.gelu(y), shape[::-1])
+        return x + MlpBlock(nn.LayerNorm()(x))
 
 
 class MlpMixer(nn.Module):