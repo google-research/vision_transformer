c7810fa8cd0118230839ed5a2e7cb0463630d7a1
AK391 <ahsengradio@gmail.com>
2021-05-12 22:45:26 -0400

    label

--- gradio/demo.py
+++ gradio/demo.py
@@ -2,6 +2,7 @@ from transformers import ViTFeatureExtractor, ViTForImageClassification
 from PIL import Image
 import requests
 import gradio as gr
+import ast
 
 feature_extractor = ViTFeatureExtractor.from_pretrained('google/vit-base-patch16-224')
 model = ViTForImageClassification.from_pretrained('google/vit-base-patch16-224')
@@ -10,13 +11,14 @@ def vitinf(image):
     inputs = feature_extractor(images=image, return_tensors="pt")
     outputs = model(**inputs)
     logits = outputs.logits
-    # model predicts one of the 1000 ImageNet classes
     predicted_class_idx = logits.argmax(-1).item()
-    return model.config.id2label[predicted_class_idx]
+    dout = ast.literal_eval("{'Predicted class' : "+ f'"{model.config.id2label[predicted_class_idx]}"'+"}")
+    return dout['Predicted class']
 
 inputs = gr.inputs.Image(type='pil', label="Original Image")
 
-outputs = gr.outputs.Textbox(label="Output Text")
+
+outputs = gr.outputs.Label(type="auto", label="Predicted class")
 
 title = "VIT"
 description = "demo for Google VIT. To use it, simply upload your image, or click one of the examples to load them. Read more at the links below."
