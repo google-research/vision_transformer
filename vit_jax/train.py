a69b207d4df07a88b1b78fa5925e6f3da9726a6a
Luke <39779310+ClashLuke@users.noreply.github.com>
2021-06-21 22:07:54 +0200

    style: revert train.py as it's unchanged

--- vit_jax/train.py
+++ vit_jax/train.py
@@ -34,37 +34,38 @@ from vit_jax import utils
 
 
 def make_update_fn(*, apply_fn, accum_steps, lr_fn):
-    """Returns update step for data parallel training."""
+  """Returns update step for data parallel training."""
 
-    def update_fn(opt, step, batch, rng):
-        _, new_rng = jax.random.split(rng)
-        # Bind the rng key to the device id (which is unique across hosts)
-        # Note: This is only used for multi-host training (i.e. multiple computers
-        # each with multiple accelerators).
-        dropout_rng = jax.random.fold_in(rng, jax.lax.axis_index('batch'))
+  def update_fn(opt, step, batch, rng):
 
-        def cross_entropy_loss(*, logits, labels):
-            logp = jax.nn.log_softmax(logits)
-            return -jnp.mean(jnp.sum(logp * labels, axis=1))
+    _, new_rng = jax.random.split(rng)
+    # Bind the rng key to the device id (which is unique across hosts)
+    # Note: This is only used for multi-host training (i.e. multiple computers
+    # each with multiple accelerators).
+    dropout_rng = jax.random.fold_in(rng, jax.lax.axis_index('batch'))
 
-        def loss_fn(params, images, labels):
-            logits = apply_fn(
-                dict(params=params),
-                rngs=dict(dropout=dropout_rng),
-                inputs=images,
-                train=True)
-            return cross_entropy_loss(logits=logits, labels=labels)
+    def cross_entropy_loss(*, logits, labels):
+      logp = jax.nn.log_softmax(logits)
+      return -jnp.mean(jnp.sum(logp * labels, axis=1))
 
-        l, g = utils.accumulate_gradient(
-            jax.value_and_grad(loss_fn), opt.target, batch['image'], batch['label'],
-            accum_steps)
-        g = jax.tree_map(lambda x: jax.lax.pmean(x, axis_name='batch'), g)
-        l = jax.lax.pmean(l, axis_name='batch')
+    def loss_fn(params, images, labels):
+      logits = apply_fn(
+          dict(params=params),
+          rngs=dict(dropout=dropout_rng),
+          inputs=images,
+          train=True)
+      return cross_entropy_loss(logits=logits, labels=labels)
 
-        opt = opt.apply_gradient(g, learning_rate=lr_fn(step))
-        return opt, l, new_rng
+    l, g = utils.accumulate_gradient(
+        jax.value_and_grad(loss_fn), opt.target, batch['image'], batch['label'],
+        accum_steps)
+    g = jax.tree_map(lambda x: jax.lax.pmean(x, axis_name='batch'), g)
+    l = jax.lax.pmean(l, axis_name='batch')
 
-    return jax.pmap(update_fn, axis_name='batch', donate_argnums=(0,))
+    opt = opt.apply_gradient(g, learning_rate=lr_fn(step))
+    return opt, l, new_rng
+
+  return jax.pmap(update_fn, axis_name='batch', donate_argnums=(0,))
 
 
 def train_and_evaluate(config: ml_collections.ConfigDict, workdir: str):
